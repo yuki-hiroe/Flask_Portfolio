2ffe48dc348d6ca8368445ad5d07f00f039d7638
yuki-hiroe <yuhiro0331@gmail.com>
2025-09-27 02:57:53 +0900

    プロフィール写真を変更

--- app.py
+++ app.py
@@ -423,4 +423,4 @@ if __name__ == '__main__':
         init_db()
     else:
         port = int(os.environ.get('PORT', 5000))
-        app.run(host='0.0.0.0', port=port)
+        app.run(host='0.0.0.0', port=port, debug=True)
